46bf0e404048fea72c28f19e89e266328bddc6df
YossefHassan19 <yossef.hassan678@gmail.com>
2026-08-23 18:50:03 +0300

    Part01 Question02

--- Program.cs
+++ Program.cs
@@ -18,6 +18,25 @@
 
             #endregion
 
+            #region Part01 Question02
+
+            //a)What is a Shallow Copy?
+            //Shallow copy creates a new object and copies all value type fields, for refrence type fields only the refrence are copied
+
+            //b)What is a Deep Copy?
+            //Deep copy creates new object and copies all nested objects, the original and the copy are completely independent
+
+            //c)What happens to reference-type members when a Shallow Copy is created?
+            //Refrence type fields only the refrence are copied, both objects point to the same nested objects
+
+            //d)What happens to reference-type members when a Deep Copy is created?
+            //Refrence type fields creates new object and the original and the copy are completely independent
+
+            //e)Give one situation where Deep Copy would be safer than Shallow Copy.
+            //Deep copy better when managing senstive refrence objects
+
+            #endregion
+
         }
     }
 }
