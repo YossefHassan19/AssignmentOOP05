a3d4d54cb6a865030bc6ad5088f3a2a9f146cff8
YossefHassan19 <yossef.hassan678@gmail.com>
2026-08-23 18:28:16 +0300

    Part01 Question01

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@
     {
         static void Main(string[] args)
         {
+            #region Part01 Question01
+
+            //a)What happens when you assign one object variable to another object variable?
+            //Copy only the refrence on stack and both variable point to same instance in memory
+
+            //b)Does assigning one object to another create a new object? Explain.
+            //No, it points to the the existing heap allocation
+
+            //c)What is the difference between copying an object and copying its reference?
+            //Copying refrence duplicates the memory address pointing to object 
+            //Copying an object allocates a new instance in heap memory with duplicate values
+
+            #endregion
 
         }
     }
